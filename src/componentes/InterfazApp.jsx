48d7b2f84bc2d47335cb335330a76f7340617ffe
Gabriela1030 <157629992+Gabriela1030@users.noreply.github.com>
2024-08-15 21:18:36 -0500

    Update InterfazApp.jsx

--- src/componentes/InterfazApp.jsx
+++ src/componentes/InterfazApp.jsx
@@ -6,7 +6,6 @@ const InterfazApp = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Aquí puedes agregar la lógica para limpiar el estado de sesión, si es necesario.
     navigate('/login');
   };
 
